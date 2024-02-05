65205ff75e8ccc32f2f0796ae00141fe197875de
pchmn <pchmn.dev@gmail.com>
2024-02-05 12:45:08 +0100

    fix: use endsWith instead of includes

--- app/routes/sitemap[.]xml.ts
+++ app/routes/sitemap[.]xml.ts
@@ -3,11 +3,12 @@ import { routes } from "@remix-run/dev/server-build";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 
 export function loader({ request }: LoaderFunctionArgs) {
+  console.log('routes', routes)
   // Remove the _layout parent id from the routes
   // Uncomment to fix extra slashes in the sitemap
   // for (const key in routes) {
   //   const route = routes[key];
-  //   if (route.parentId?.includes('_layout')) {
+  //   if (route.parentId?.endsWith('_layout')) {
   //     route.parentId = routes[route.parentId].parentId;
   //   }
   // }
