65da3ab37028b24f10a74bb79b1d3f1e68dc7029
Paul Chemin <pchmn.dev@gmail.com>
2024-02-05 12:26:13 +0100

    Add sitemap extra slashes bug

---
+++ app/routes/_extra-slahes+/extra-slashes.tsx
@@ -0,0 +1,7 @@
+export default function ExtraSlashesRoute() {
+  return (
+    <div>
+      <h1>Extra Slashes Route</h1>
+    </div>
+  );
+}
--- app/routes/sitemap[.]xml.ts
+++ app/routes/sitemap[.]xml.ts
@@ -1,8 +1,16 @@
+import { generateSitemap } from "@nasa-gcn/remix-seo";
 import { routes } from "@remix-run/dev/server-build";
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { generateSitemap } from "@nasa-gcn/remix-seo";
 
 export function loader({ request }: LoaderFunctionArgs) {
+  // Remove the _layout parent id from the routes
+  // Uncomment to fix extra slashes in the sitemap
+  // for (const key in routes) {
+  //   const route = routes[key];
+  //   if (route.parentId?.includes('_layout')) {
+  //     route.parentId = routes[route.parentId].parentId;
+  //   }
+  // }
   return generateSitemap(request, routes, {
     siteUrl: "https://balavishnuvj.com",
   });
