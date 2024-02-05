85d8852bacbc79d606abf5c4c6691e18db3bf567
Paul Chemin <pchmn.dev@gmail.com>
2024-02-05 17:19:00 +0100

    feat: add more routes

---
+++ app/routes/_double-slahes+/_triple-slashes+/triple-slashes.tsx
@@ -0,0 +1,7 @@
+export default function ExtraSlashes2Route() {
+  return (
+    <div>
+      <h1>Extra Slashes 2 Route</h1>
+    </div>
+  );
+}
--- app/routes/sitemap[.]xml.ts
+++ app/routes/sitemap[.]xml.ts
@@ -2,17 +2,9 @@ import { generateSitemap } from "@nasa-gcn/remix-seo";
 import { routes } from "@remix-run/dev/server-build";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 
+
 export function loader({ request }: LoaderFunctionArgs) {
-  console.log('routes', routes)
-  // Remove the _layout parent id from the routes
-  // Uncomment to fix extra slashes in the sitemap
-  // for (const key in routes) {
-  //   const route = routes[key];
-  //   if (route.parentId?.endsWith('_layout')) {
-  //     route.parentId = routes[route.parentId].parentId;
-  //   }
-  // }
   return generateSitemap(request, routes, {
-    siteUrl: "https://balavishnuvj.com",
+    siteUrl: "https://example.com",
   });
 }
